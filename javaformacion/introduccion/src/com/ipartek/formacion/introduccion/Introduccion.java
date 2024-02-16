eccc997df9dbf3e696360ed9129f1a5311aff246
yordytafur <yordytp10@gmail.com>
2024-02-16 08:27:12 +0100

    primer repositorio

---
+++ javaformacion/introduccion/src/com/ipartek/formacion/introduccion/Introduccion.java
@@ -0,0 +1,14 @@
+package com.ipartek.formacion.introduccion;
+
+public class Introduccion {
+
+	public static void main(String[] args) {
+
+	System.out.println("Hola capullo");
+	System.out.println("Venga nene");
+	System.out.println("Lo que digas");
+//	Ctrl + 7 para comentario de línea;
+	
+	}
+
+}
